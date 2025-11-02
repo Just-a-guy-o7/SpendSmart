dcdff744ffcee294acfecc85e0d9a2adf96db9f3
Just-a-guy-o7 <pandey1divyanshu@gmail.com>
2025-11-03 00:25:30 +0530

    Added the base template for spring boot and made a temparary test page

---
+++ spendSmart/src/test/java/com/spendSmart/spendSmart/SpendSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendSmart.spendSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
